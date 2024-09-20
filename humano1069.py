94ab4af7c62ee88ef73fbf1d53630a5e31937ce7
Alex1ssss <alexissanchez99998@gmail.com>
2024-09-20 11:51:40 -0600

    Add files via upload

---
+++ humano1069.py
@@ -0,0 +1,65 @@
+print("act 9 clase humano")
+print("Alexis Espino Sanchez mat: 22308051281062")
+# zona de clases 
+class Humano1062:
+    #zona de atributos dentro de la clase
+    edad=0
+    peso=0
+    estatura=0
+    colordepelo=0
+    colordeojos=0
+    numerodealzado=0
+    #zona de funciones dentro de la clase
+    def correr1062(self,n):
+        print(f"{n} esta corriendo")
+    def sonrie1062(self,n):
+        print(f"{n} esta sonriendo")
+    def come1062(self,n):
+        print(f"{n} esta comiendo")  
+    def estudia1062(self,n):
+        print(f"{n} esta estudiando")  
+    def juega1062(self,n):
+        print(f"{n} esta jugando")  
+            
+#zona de creacion de objetos
+alexis=Humano1062()
+Joanna=Humano1062()
+#zona de usando objetos
+print("Resultados para alexis")
+alexis.edad=17
+alexis.peso=45
+alexis.estatura=168
+alexis.colordepelo="negro"
+alexis.colordeojos="cafes"
+alexis.numerodealzado=27
+
+print(f"edad: {alexis.edad}")
+print(f"peso: {alexis.peso}")
+print(f"estatura: {alexis.estatura}")
+print(f"color de pelo: {alexis.colordepelo}")
+print(f"color de ojos: {alexis.colordeojos}")
+print(f"numero de calzado: {alexis.numerodealzado}")
+alexis.correr1062("alexis")
+alexis.sonrie1062("alexis")
+alexis.come1062("alexis")
+alexis.estudia1062("alexis")
+alexis.juega1062("alexis")
+
+print("Resultados para joanna")    
+Joanna.edad=18
+Joanna.peso=52
+Joanna.estatura=155
+Joanna.colordepelo="cafe"
+Joanna.colordeojos="cafes"
+Joanna.numerodealzado=23
+print(f"edad: {Joanna.edad}")
+print(f"peso: {Joanna.peso}")
+print(f"estatura: {Joanna.estatura}")
+print(f"color de pelo: {Joanna.colordepelo}")
+print(f"color de ojos: {Joanna.colordeojos}")
+print(f"numero de calzado: {Joanna.numerodealzado}")
+Joanna.correr1062("Joanna")
+Joanna.sonrie1062("Joanna")
+Joanna.come1062("Joanna")
+Joanna.estudia1062("Joanna")
+Joanna.juega1062("Joanna")
